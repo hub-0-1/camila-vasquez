9bf153223846d37a2d81afa580ddbb73a28e8a97
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-07-28 14:34:27 -0400

    apparition des images

--- animation.js
+++ animation.js
@@ -1,7 +1,44 @@
+var config = {
+  images: [
+    'madonna1.jpg',
+    'madonna2.jpg',
+    'madonna3.jpg',
+    'madonna4.jpg',
+    'madonna5.jpg',
+    'madonna6.jpg',
+    'madonna7.jpg',
+    'madonna8.jpg',
+    'madonna9.jpg',
+    'madonna10.jpg'
+  ]
+}
+
 window.onload = function () {
 
-  let container = document.getElementById("animation-accueil");
-  container.addEventListener("wheel", function (e) { 
-    console.log(e); 
+  let canva = document.getElementById("animation-accueil");
+
+  canva.addEventListener("wheel", function (e) { 
+    let bounding_rect_animation = e.target.getBoundingClientRect();
+
+    let image = creer_image(src_image_aleatoire());
+    image.style.top = 10 + "px";
+    image.style.left = 10 + "px";
+
+    canva.appendChild(image);
+    console.log(e);
   });
+}
+
+function creer_image (src) {
+  let img = document.createElement("img");
+  img.src = "images/" + src;
+  img.className = "image-flottante"
+  img.alt = "Madonna";
+
+  return img; 
+}
+
+function src_image_aleatoire () {
+  let index = Math.round(Math.random() * (config.images.length - 1));
+  return config.images[index];
 }
