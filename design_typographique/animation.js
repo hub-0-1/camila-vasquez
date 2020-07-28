7bca11e5e2efb3b6165be38607c9ab736c29afe7
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-07-28 10:08:25 -0400

    page design

--- design_typographique/animation.js
+++ design_typographique/animation.js
@@ -1,5 +1,26 @@
+var rotation_scroller = 0;
+var vitesse_rotation = 1.3;
+
 window.onload = function () {
   document.getElementById("animation-design-typographique").addEventListener("wheel", function (e) { 
-    console.log(e); 
-  })
+    let direction = Math.sign(e.deltaX);
+
+    // Pas de scroll en X
+    if(direction == 0) {
+      return;
+    }
+    else if(direction == 1) {
+      rotation_scroller += 1;
+      set_rotation (rotation_scroller);
+    }
+    else if(direction == -1) {
+      rotation_scroller -= 1;
+      set_rotation (rotation_scroller);
+    }
+  });
+}
+
+function set_rotation (rotation) {
+  document.getElementById("element-transformation").setAttribute("transform", 
+    "rotate(" + rotation * vitesse_rotation + ", 60, 60)");
 }
