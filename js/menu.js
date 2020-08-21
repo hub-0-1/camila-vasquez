9d492984dcd48dd9ffc29b66b2940eb164af811f
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-08-21 16:12:10 -0400

    refact

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,14 @@
 function init_menu () {
   document.getElementById("hamburger").addEventListener("click", () => {
-    [].forEach.call(document.querySelectorAll("#menu"), (el) => {
-      if([].includes.call(el.classList, "show")) {
-        el.classList.remove("show");
-      }
-      else {
-        el.classList.add("show");
-      }
-    });
+
+    let el = document.getElementById("menu");
+
+    if([].includes.call(el.classList, "show")) {
+      el.classList.remove("show");
+    }
+    else {
+      el.classList.add("show");
+    }
+
   });
 }
