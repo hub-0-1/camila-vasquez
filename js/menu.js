00e04c4ba9d5bae7a230a234c1a8c795060284c7
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-08-21 15:51:07 -0400

    menu page accueil

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 function init_menu () {
   document.getElementById("hamburger").addEventListener("click", () => {
-    [].forEach.call(document.querySelectorAll(".item-menu"), (el) => {
+    [].forEach.call(document.querySelectorAll("#menu"), (el) => {
       if([].includes.call(el.classList, "show")) {
         el.classList.remove("show");
       }
