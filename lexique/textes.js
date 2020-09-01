d05fa89185314e7428037355e5846c571dff468c
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-09-01 14:15:24 -0400

    textes fr lexique

--- lexique/textes.js
+++ lexique/textes.js
@@ -1,9 +1,85 @@
 var textes = {
-  appartenir: { 
-    original: "$appartenir$ @v. intr. . conjug.@ _1._ %Faire partie de (qqch.).% *Appartenir à une vielle famille du pays.*",
-    interpretation: "$appartenir à une communauté$ _1._ %Formule alternative ou complémentaire à la famille nucléaire, qui propose une possible solution à la solitude et l'isolement de l'individu, à la séparation des couples et à la dissolution des liens des familles élargies, phénomènes de l'ère actuelle, caractéristiques des sociétés capitalistes.%" 
+  fr: {
+    appartenir: {
+      mot: "Appartenir",
+      original: "$appartenir$ @v. intr. . conjug.@ _1._ %Faire partie de (qqch.).% *Appartenir à une vielle famille du pays.*",
+      interpretation: "$appartenir à une communauté$ _1._ %Formule alternative ou complémentaire à la famille nucléaire, qui propose une possible solution à la solitude et l'isolement de l'individu, à la séparation des couples et à la dissolution des liens des familles élargies, phénomènes de l'ère actuelle, caractéristiques des sociétés capitalistes.%"
+    },
+    apnee: {
+      mot: "Apnée",
+      original: "$apnée$ @n. f.@ _1._ %Arrêt temporaire de la respiration.%",
+      interpretation: "$en apnée$ _1._ %État psychologique qui se caractérise par un mouvement intérieur permettant à l'individu de disparaître, de devenir invisible, quand il le juge nécessaire ou utile.%"
+    },
+    breche: {
+      mot: "Brèche",
+      original: "$brèche$ @n. f.@ _1._ %Petite entaille sur un objet d’où s'est détaché un éclat.%",
+      interpretation: "$saisir les brèches dans le temps$@loc.@ _1._ %Possibilité qui se présente soudainement à un parent de s'emparer d'un moment précis au milieu d'une continuité indéfinie d’obligations prioritaires (ex. préparer le repas, payer les factures, remplir l'autorisation de la sortie scolaire, etc.), pour laisser place à l’intuition.%"
+    },
+    chambouler: {
+      mot: "Chambouler",
+      original: "$chambouler$ @v. tr. . conjug.@_1._@Fam.@ %Bouleverser, mettre sens dessus dessous.%",
+      interpretation: "$chambouler l'hétérosexualité$ _1._ %Durant l'acte sexuel, changer les position, les modes, les fonctions des partenaires hétérosexuels.%_2._%Renverser le sens, les habitudes et les comportements sexuels liés à la satisfaction des besoins érotiques entre femme et homme.%_3._%Manière de subvertir le système de pouvoir du patriarcat dans le lit.%"
+    },
+    egal: {
+      mot: "Être égaux",
+      original: "$égal, ale, aux$ @adj. et n.@ _1._ @(Personnes, choses)@ %Qui est de la même quantité, dimension, nature, qualité ou valeur.% _identique, même ; équivalent._ @/ contr.@ _inégal ; différent._ _2._ @(Personnes)@ %Qui es sur le même rang ; qui a les mêmes droits et charges% _pareil._",
+      interpretation: "$être égaux$ _1._ @loc.@ %Dans un couple hétérosexuel, partager de manière équitable autant les tâches domestiques que la charge mentale – qui peut être définie comme  « le travail de gestion, d’organisation et de planification qui est à la fois intangible, incontournable et constant, et qui a pour objectifs la satisfaction des besoins de chacun et la bonne marche de la résidence. ». – et ce, malgré la contradiction flagrante qui existe entre le discours et la pratique quant à l’égalité entre hommes et femmes à la maison.%"
+    },
+    faire: {
+      mot: "Faire",
+      original: "$faire$ @v. tr. . conjug.@ _1._ %Réaliser hors de soi (une chose matérielle).% *Faire une maison.* _bâtir, construire._ *Faire un meuble.* _fabriquer_ *Faire le pain.* _préparer_ *Faire une robe.* *>* _confectionner._ *Faire un tableau.* _créer._ _2._ %Réaliser (une chose abstraite).% *Faire une loi.* _instaurer, instituer._ *Faire une oeuvre.* _composer; créer._ *Faire un poème, une sonate.* _écrire._ ",
+      interpretation: "$faire ce qu'on peut et en faire le moins possible$ _1._ %Stratégie d'adaptation de parents de familles nombreuses qui a comme objectif de préserver une bonne santé mentale et, à la fois, subvenir aux besoins de leurs enfants.%"
+    },
+    fissurer: {
+      mot: "Fissurer",
+      original: "$fissurer$ @v. tr. . conjug.@ _1._ %Diviser par fissures.% _crevasser, fendre._",
+      interpretation: "$fissurer la monogamie$ _1._ %Démonter la hiérarchie des relations humaines qui situe l’amoureux ou l’amoureuse au sommet de la pyramide au détriment d'autres liens de parenté, de famille, d'amitié, de voisinage, etc.%"
+    },
+    inverser: {
+      mot: "Inverser",
+      original: "$inverser$ @v. tr. . conjug.@ _1._ %Changer la position, l’ordre. >% _intervertir._ ",
+      interpretation: "$inverser les rôles$ _1._ %Dans un couple hétérosexuel, entente explicite ou implicite selon laquelle l'homme s'occupe de tâches considérées traditionnellement comme étant féminines (ex. prendre soin des enfants) et la femme se consacre au travail intellectuel, manuel ou physique ou toute tâche considérée habituellement comme étant masculine (ex. recherche scientifique).% _2._ %Modalité qui présente l'avantage de développer l'empathie entre les partenaires.% *Père au foyer et mère pourvoyeuse.*"
+    },
+    jouer: {
+      mot: "Jouer",
+      original: "$jouer$ @v. tr. . conjug.@ _1._ %Se livrer à un jeu.% @s’amuser.@",
+      interpretation: "$jouer entre adultes$ _1._ %Activité qui diminue en fréquence et en intensité ou qui disparaît complètement chez l'humain durant le passage à l'âge adulte, empêchant l'individu de retirer les bienfaits que procure ladite activité, tels que : l'amusement, la découverte, l'imagination, l’apprentissage, l'inspiration, la créativité, la concentration d'esprit, la distraction, l'excitation, la joie, la relaxation et la stimulation, pour n’en nommer que quelques uns.%"
+    },
+    mistifi: {
+      mot: "Mistifi",
+      original: "$Mistifi$ @n.p.@ _1._ %Société idéale située dans une géographie imaginaire où de multiples utopies de la famille sont possibles et coexistent.% *À bas l’image de la famille parfaite!*",
+      interpretation: "$Mistifi$ @n.p.@ _1._ %Société idéale située dans une géographie imaginaire où de multiples utopies de la famille sont possibles et coexistent.% *À bas l’image de la famille parfaite!*"
+    },
+    montagne: {
+      mot: "Montagne",
+      original: "$montagne$ @n.f.@ _1._ %Importante élévation du terrain.% _éminence, hauteur;mont._ _2._ @loc.@ %Métaphore qui désigne la famille comme étant ce qui occupe la plus grande partie de l'espace dans la maison et la vie, tout en étant aussi ce qu'il y a de plus beau.%",
+      interpretation: "$montagne$ @n.f.@ _1._ %Importante élévation du terrain.% _éminence, hauteur;mont._ _2._ @loc.@ %Métaphore qui désigne la famille comme étant ce qui occupe la plus grande partie de l'espace dans la maison et la vie, tout en étant aussi ce qu'il y a de plus beau.%"
+    },
+    presence: {
+      mot: "Présence",
+      original: "$présence$ @n.f.@ _1._ %Le fait d'être physiquement quelque part auprès de qqn.% @/contr.@  _abscence._ _2._ %Disposition d'esprit qui permet d'avoir accès à l'amour envers les membres de sa famille, au delà du surmenage, des exigences de la vie quotidienne, des soucis ou de la cohabitation.%",
+      interpretation: "$présence$ @n.f.@ _1._ %Le fait d'être physiquement quelque part auprès de qqn.% @/contr.@  _abscence._ _2._ %Disposition d'esprit qui permet d'avoir accès à l'amour envers les membres de sa famille, au delà du surmenage, des exigences de la vie quotidienne, des soucis ou de la cohabitation.%"
+    },
+    refugier: {
+      mot: "Se réfugier",
+      original: "$se réfugier$ @v. tr. . conjug.@ _1._ %Se retirer (en un lieu) pour s'y mettre à l'abri.%",
+      interpretation: "$se réfugier dans la salle de bain$ _1._ %S'isoler dans la salle de bain à des fins qui ne sont pas habituelles, pour fuir la famille et retrouver un espace individuel ou de couple.%"
+    },
+    travailler: {
+      mot: "Travailler",
+      original: "$travailler$ @v.  . conjug.@ _1._ %Agir d'une manière suivie, avec plus ou moins d'effort, pour obtenir un résultat utile (intellectuellement, manuellement).% @fam.@ _Bosser, boulonner_ @/ contr.@ _s’amuser, chômer, se reposer._ _2._ %Exercer une activité professionnelle, un métier.%",
+      interpretation: "$ne jamais travailler$ _1._ %Acte de résistance au capitalisme qui consiste à rééquilibrer la vie personnelle – et, éventuellement, la société – en partageant son temps entre vie familiale et amicale, vie politique, vie spirituelle et des activités individuelles, et en refusant de se consacrer à la vie productive, qui occupe trop d'importance dans le monde actuel pour une grande majorité des adultes.%"
+    },
+    regarder: {
+      mot: "Regarder",
+      original: "$travailler$ @v.  . conjug.@ _1._ %Agir d'une manière suivie, avec plus ou moins d'effort, pour obtenir un résultat utile (intellectuellement, manuellement).% @fam.@ _Bosser, boulonner_ @/ contr.@ _s’amuser, chômer, se reposer._ _2._ %Exercer une activité professionnelle, un métier.%",
+      interpretation: "$regarder la maison de l'intérieur$ _1._ @loc.@ %Faculté d'examiner sa maison quand elle en désordre sans éprouver de l'agitation et, au contraire, irradier un faisceau lumineux paisible qui reflète de l’harmonie dans l’espace habité.%"
+    }
+  },
+  en: {
+
   }
-} 
+}
 
 function parser (texte) {
   texte = texte.replace(/%([-'0-9a-zÀ-ÿ'\s\.\(\),]+)%/gi, "<span class='baskerville'>$1</span>");
@@ -20,12 +96,20 @@ function parser_entree (texte) {
 }
 
 function traiter_textes () {
-  let textes_traites = {};
+  let textes_traites = {
+    fr: {},
+    en: {}
+  };
 
-  Object.keys(textes).map((entree) => {
-    textes_traites[entree] = parser_entree(textes[entree]);
+  // Francais
+  Object.keys(textes.fr).map((entree) => {
+    textes_traites.fr[entree] = parser_entree(textes.fr[entree]);
+  });
+
+  // Anglais
+  Object.keys(textes.en).map((entree) => {
+    textes_traites.en[entree] = parser_entree(textes.en[entree]);
   });
 
   return textes_traites;
 }
-
