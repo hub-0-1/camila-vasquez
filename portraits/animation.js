ee1a10a145121b3673293eff3b6b5e36187d3698
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-09-18 15:19:28 -0400

    portritats

--- portraits/animation.js
+++ portraits/animation.js
@@ -1,3 +1,17 @@
 window.onload = function () {
   document.getElementById("legende").style.width = document.getElementById("animation-portraits").style.width;
+
+  [].forEach.call(document.querySelectorAll(".portrait"), (image) => {
+    image.addEventListener("click", toggleFocus.bind(image));
+  });
+
+  [].forEach.call(document.querySelectorAll(".paysage"), (image) => {
+    image.addEventListener("click", toggleFocus.bind(image));
+  });
 }
+
+function toggleFocus () {
+  this.classList.toggle("focus");
+  document.querySelector("#portraits").style.transform = "translateX(-600px)";
+}
+
