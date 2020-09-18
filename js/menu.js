56c3be4a2a71d9c9ea6502dcc9f996dc56ff8900
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-09-18 17:21:46 -0400

    debut opacity menu

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,17 @@ function init_menu () {
   document.getElementById("hamburger").addEventListener("click", () => {
 
     let el = document.getElementById("menu");
+    let main = document.getElementsByTagName("main")[0];
 
     if([].includes.call(el.classList, "show")) {
-      el.classList.remove("show");
+      main.style.opacity = 1;
+      el.style.opacity = 0;
+      window.setTimeout(function () { el.classList.remove("show"); }, 2000);
     }
     else {
+      main.style.opacity = 0;
       el.classList.add("show");
+      window.setTimeout(function () { el.style.opacity = 1; }, 1);
     }
 
   });
