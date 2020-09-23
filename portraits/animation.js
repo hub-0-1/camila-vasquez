cbc2f2464b63bdbb6676b8babccef550927a6e19
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-09-23 08:36:10 -0400

    portraits finaux

--- portraits/animation.js
+++ portraits/animation.js
@@ -1,11 +1,11 @@
 window.onload = function () {
+
+  init_menu();
+  init_traduction();
+
   document.getElementById("legende").style.width = document.getElementById("animation-portraits").style.width;
 
-  [].forEach.call(document.querySelectorAll(".portrait"), (image) => {
-    image.addEventListener("click", toggleFocus.bind(image));
-  });
-
-  [].forEach.call(document.querySelectorAll(".paysage"), (image) => {
+  [].forEach.call(document.querySelectorAll(".image"), (image) => {
     image.addEventListener("click", toggleFocus.bind(image));
   });
 }
@@ -13,6 +13,12 @@ window.onload = function () {
 function toggleFocus () {
   this.classList.toggle("focus");
   if([].includes.call(this.classList, "focus")) {
+
+    [].forEach.call(document.querySelectorAll(".image"), (image) => {
+      if(image == this) return;
+      image.classList.remove("focus");
+    });
+
     document.querySelector("#animation-portraits").scroll({
       top: 0,
       left: this.offsetLeft - ((window.innerWidth / 2) - (this.offsetWidth / 2)),
