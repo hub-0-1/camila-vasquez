19ecb1438e68a77b78e3395d67d18aa24b6ef396
Olivier Ross <olivier.ross@usherbrooke.ca>
2021-02-04 14:59:05 -0500

    images legeres + menu

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,25 @@
 function init_menu () {
   let menu = document.createElement("div");
   let info = document.createElement("div");
-  menu.innerHTML = `<div id="menu">
-      <div class="item-menu"><div class="index">01.</div><a class="oeuvre" href="/lexique/" data-i18n="header.lexique"></a></div>
-      <div class="item-menu"><div class="index">02.</div><a class="oeuvre" href="/collage/" data-i18n="header.collage"></a></div>
-      <div class="item-menu"><div class="index">03.</div><a class="oeuvre" href="/temps_ensemble/" data-i18n="header.design_typo"></a></div>
-      <div class="item-menu"><div class="index">04.</div><a class="oeuvre" href="/portraits/" data-i18n="header.portraits"></a></div>
-      <div class="item-menu"><div class="index">05.</div><a class="oeuvre" href="/tableau_blanc/" data-i18n="header.tableau_blanc"></a></div>
-      <div class="item-menu"><div class="index">06.</div><a class="oeuvre" href="#info" data-i18n="header.info" onclick="afficher_info();"></a></div>
+  menu.innerHTML = `
+    <div id="menu">
+      <div class="wrapper-menu">
+        <div class="item-menu"><div class="index">01.</div><a class="oeuvre" href="/lexique/" data-i18n="header.lexique"></a></div>
+        <div class="item-menu"><div class="index">02.</div><a class="oeuvre" href="/collage/" data-i18n="header.collage"></a></div>
+        <div class="item-menu"><div class="index">03.</div><a class="oeuvre" href="/temps_ensemble/" data-i18n="header.design_typo"></a></div>
+        <div class="item-menu"><div class="index">04.</div><a class="oeuvre" href="/portraits/" data-i18n="header.portraits"></a></div>
+      </div>
+      <div class="wrapper-menu">
+        <div class="item-menu"><div class="index">05.</div><a class="oeuvre" href="/tableau_blanc/" data-i18n="header.tableau_blanc"></a></div>
+        <div class="item-menu"><div class="index">06.</div><a class="oeuvre" href="/retour/" data-i18n="header.retour"></a></div>
+        <div class="item-menu"><div class="index">07.</div><a class="oeuvre" href="#info" data-i18n="header.info" onclick="afficher_info();"></a></div>
+      </div>
     </div>
 
     <a id="container-logo" href="/"><img id="logo" class="icone" src="/images/logo.png"></a>
     <div id="langue" data-i18n="header.autre_langue" data-langue="fr"></div>
-    <div id="hamburger">Menu</div>`;
+    <div id="hamburger">Menu</div>
+   `;
 
   info.id = "animation-info";
   info.className = "animation";
